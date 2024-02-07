4873410e09d3312d9a99871836cc9bccd9282286
fellipe27 <felipe27pffl@gmail.com>
2024-02-07 15:51:15 -0300

    add tests

--- src/main/java/com/codex/aposta/model/dto/ApostaIn.java
+++ src/main/java/com/codex/aposta/model/dto/ApostaIn.java
@@ -1,9 +1,13 @@
 package com.codex.aposta.model.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApostaIn {
     @NotNull
     private Long idApostador;
